8622f7a0e556371a30baa175bc19f283451e247f
alumnog <alumnog@S921-6.itson.edu.mx>
2025-03-13 10:28:56 -0700

    Se agregó el Main

---
+++ src/mx/itson/music/ui/Main.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.music.ui;
+
+import java.util.ArrayList;
+import java.util.List;
+import mx.itson.music.entidades.Album;
+import mx.itson.music.entidades.Artista;
+import mx.itson.music.entidades.Canción;
+import mx.itson.music.enums.Género;
+
+/**
+ *
+ * @author alumnog
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Artista artista = new Artista();
+        artista.setNombre("Bad Bunny");
+        artista.setSitioWeb("www.badbunny.com");
+        
+        Album a = new Album();
+        a.setNombre("Debí tirar más fotos");
+        a.setLanzamiento(2025);
+        a.setArtista(artista);
+        a.setGénero(Género.OTRO);
+        
+        List<Canción> canciones = new ArrayList<>();
+       
+        
+    }
+}
